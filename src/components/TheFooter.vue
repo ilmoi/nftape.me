fc1f0a93fea6f47778156d72f91433be34656052
ilmoi <iljamoi@protonmail.com>
2021-12-09 21:54:17 +0200

    move to gg

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="footer">
-    <p class="px-3">built with</p>
+  <div class="footer pt-1 pb-1">
+    <p class="px-3">Built with</p>
     <i class="nes-icon heart"></i>
-    <p class="px-3">by <a href="https://twitter.com/_ilmoi" target="_blank">@_ilmoi</a></p>
+    <p class="px-3">
+      by
+      <a href="https://twitter.com/_ilmoi" target="_blank">@_ilmoi</a>
+      | powered by <a href="https://twitter.com/GenesysGo" target="_blank">GenesysGo</a>
+    </p>
+    <img src="../assets/gg_logo.png" height="31" width="28" />
   </div>
 </template>
 
